1c28c3c00c7ca3e55ece9be3036d9395f61a52ab
tomasbudrikas10 <tomasbudrikas10@gmail.com>
2022-02-24 04:42:03 +0200

    blablabla

--- Egzaminas/resources/views/index.blade.php
+++ Egzaminas/resources/views/index.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
     <h1 class="text-5xl text-center">Autoservice Experts</h1>
-    <div>
+    <div class="grid grid-cols-3 gap-2">
         @foreach ($experts as $expert)
-            <div>
+            <div class="mx-auto">
+                <img src="#" alt="Image goes here." class="w-52 h-52">
                 <p class="text-center text-2xl font-bold">{{ $expert->name }} {{ $expert->lastname }}</p>
-                <span class="text-md text-gray-700">{{ $expert->specialization->name }}</span>
+                <span class="text-md text-gray-700 text-center">{{ $expert->specialization->name }}</span>
                 <p class="text-lg text-center pt-5">{{ $expert->autoservice->name }}</p>
             </div>
         @endforeach
