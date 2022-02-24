3f618cd91e6c479d570f2f33036de42a6c0a97b5
tomasbudrikas10 <tomasbudrikas10@gmail.com>
2022-02-24 03:23:39 +0200

    Created database seeder

--- Egzaminas/database/seeders/DatabaseSeeder.php
+++ Egzaminas/database/seeders/DatabaseSeeder.php
@@ -28,6 +28,18 @@ class DatabaseSeeder extends Seeder
         Autoservice::create(['name' => 'Šasi', 'location' => 'Paliūniškio g. 13, LT-35114 Panevėžys']);
         Autoservice::create(['name' => 'Melga', 'location' => 'Savanorių pr. 151A, LT-03150 Vilnius']);
         Autoservice::create(['name' => 'Ausegra', 'location' => 'Gelvonų g. 2, LT-07149 Vilnius']);
-        
+
+        Expert::create(['name' => 'Jonas', 'lastname' => 'Jonaitis', 'specialization_id' => '1', 'autoservice_id' => '1', 'city' => 'Klaipėda']);
+        Expert::create(['name' => 'Petras', 'lastname' => 'Petrauskis', 'specialization_id' => '2', 'autoservice_id' => '2', 'city' => 'Klaipėda']);
+        Expert::create(['name' => 'Bronius', 'lastname' => 'Bronskius', 'specialization_id' => '3', 'autoservice_id' => '3', 'city' => 'Kaunas']);
+        Expert::create(['name' => 'Darius', 'lastname' => 'Dariauskas', 'specialization_id' => '4', 'autoservice_id' => '4', 'city' => 'Vilnius']);
+        Expert::create(['name' => 'Lukas', 'lastname' => 'Lukauskas', 'specialization_id' => '5', 'autoservice_id' => '5', 'city' => 'Klaipėda']);
+        Expert::create(['name' => 'Andrius', 'lastname' => 'Andriauskas', 'specialization_id' => '2', 'autoservice_id' => '3', 'city' => 'Klaipėda']);
+        Expert::create(['name' => 'Orestas', 'lastname' => 'Orestauskas', 'specialization_id' => '3', 'autoservice_id' => '2', 'city' => 'Alytus']);
+        Expert::create(['name' => 'Kasparas', 'lastname' => 'Kasparauskas', 'specialization_id' => '4', 'autoservice_id' => '2', 'city' => 'Telšiai']);
+        Expert::create(['name' => 'Deividas', 'lastname' => 'Deividauskas', 'specialization_id' => '5', 'autoservice_id' => '1', 'city' => 'Plungė']);
+        Expert::create(['name' => 'Ignas', 'lastname' => 'Ignauskas', 'specialization_id' => '1', 'autoservice_id' => '5', 'city' => 'Ignalina']);
+
+
     }
 }
