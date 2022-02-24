4fd698e85f7e9e7079308d83c9148b1b460ac8e3
tomasbudrikas10 <tomasbudrikas10@gmail.com>
2022-02-24 04:47:42 +0200

    aaaa

--- Egzaminas/resources/views/index.blade.php
+++ Egzaminas/resources/views/index.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
     <h1 class="text-5xl text-center">Autoservice Experts</h1>
-    <div class="grid grid-cols-3 gap-2">
+    <div class="grid grid-cols-3 gap-2 p-16">
         @foreach ($experts as $expert)
             <div class="mx-auto">
-                <img src="#" alt="Image goes here." class="w-52 h-52">
+                <p>Rating: {{ $expert->rating->rating }}</p>
+                <a href="/click/{{ $expert->id }}"><p class="text-3xl text-right">♥</p></a>
+                <img src="#" alt="Image goes here." class="h-52 mx-auto">
                 <p class="text-center text-2xl font-bold">{{ $expert->name }} {{ $expert->lastname }}</p>
-                <span class="text-md text-gray-700 text-center">{{ $expert->specialization->name }}</span>
+                <p class="text-lg text-center">{{ $expert->city }}</p>
+                <p class="text-md text-gray-700 text-center">{{ $expert->specialization->name }}</p>
                 <p class="text-lg text-center pt-5">{{ $expert->autoservice->name }}</p>
             </div>
         @endforeach
