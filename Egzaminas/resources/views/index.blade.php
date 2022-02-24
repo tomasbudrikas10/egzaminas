b8a41b417b6400ce94666b8b574460883be5ba4d
tomasbudrikas10 <tomasbudrikas10@gmail.com>
2022-02-24 04:38:40 +0200

    blablabla'

--- Egzaminas/resources/views/index.blade.php
+++ Egzaminas/resources/views/index.blade.php
@@ -1,13 +1,13 @@
 @extends('base')
 
 @section('content')
-    <h1 class="text-5xl">Autoservice Experts</h1>
+    <h1 class="text-5xl text-center">Autoservice Experts</h1>
     <div>
         @foreach ($experts as $expert)
             <div>
                 <p class="text-center text-2xl font-bold">{{ $expert->name }} {{ $expert->lastname }}</p>
-                <span class="text-md text-gray-700">{{ $expert->specialization->specialization }}</span>
-                <p class="text-lg text-center">{{ $expert->autoservice->autoservice }}</p>
+                <span class="text-md text-gray-700">{{ $expert->specialization->name }}</span>
+                <p class="text-lg text-center pt-5">{{ $expert->autoservice->name }}</p>
             </div>
         @endforeach
     </div>
